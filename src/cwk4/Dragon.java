a53805eadc1a3d6810ee5e46d3e52d30f435c992
Arihaan <48653895+Arihaan@users.noreply.github.com>
2024-04-06 04:50:04 +0100

    Delete src/cwk4 directory
    Deleted the uploaded code to restart repo with initial IntelliJ config files

--- src/cwk4/Dragon.java
+++
@@ -1,9 +0,0 @@
-package cwk4;
-
-public class Dragon extends Champion{
-    boolean isTalking;
-    public Dragon(boolean isTalking, int fee, String state, int skill, boolean isMagic, boolean isFight, boolean isMystery){
-        super(fee, state, skill, isMagic, isFight, isMystery);
-        this.isTalking = isTalking;
-    }
-}
--- src/cwk4/Warrior.java
+++
@@ -1,9 +0,0 @@
-package cwk4;
-
-public class Warrior extends Champion{
-    String weapon;
-    public Warrior(String weapon, int fee, String state, int skill, boolean isMagic, boolean isFight, boolean isMystery){
-        super(fee, state, skill, isMagic, isFight, isMystery);
-        this.weapon = weapon;
-    }
-}
