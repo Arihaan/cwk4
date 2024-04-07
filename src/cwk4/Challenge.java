bc9ff39cbf6fc541dbebc9542ed33b9ec026b1b1
Arihaan <48653895+Arihaan@users.noreply.github.com>
2024-04-07 18:35:37 +0100

    Added toString method

--- src/cwk4/Challenge.java
+++ src/cwk4/Challenge.java
@@ -7,11 +7,22 @@ public class Challenge {
     private int skillLevel;
     private int reward;
 
-    public Challenge(int challengeNo, String challengeType, String enemy, int skillLevel, int reward){
+    public Challenge(int challengeNo, String challengeType, String enemy, int skillLevel, int reward)
+    {
         this.challengeNo = challengeNo;
         this.challengeType = challengeType;
         this.enemy = enemy;
         this.skillLevel = skillLevel;
         this.reward = reward;
+    }
+
+    public String toString()
+    {
+        String s;
+
+        s = "\nChallenge No.: " + challengeNo + "\nChallenge Type: " + challengeType + "\nEnemy Name: " + enemy +
+                "\nSkill Level: " + skillLevel + "\nReward: " + reward;
+
+        return s;
     }
 }
